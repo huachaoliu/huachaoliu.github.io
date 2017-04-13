f5fa8d689b70913fd57d020b369f4934565f13cd
刘华超 <huachao.liu@daxiangyun.com>
2017-04-13 18:33:17 +0800

    color picker

--- plugins/color-picker/js/color-picker.js
+++ plugins/color-picker/js/color-picker.js
@@ -25,6 +25,7 @@
         this.color_wheel = 'color-wheel';
         //进度条
         this.color_bar = 'color-bar';
+        this.color_selected = 'color-bar-selected';
         //title
         this.color_title = 'color-title';
         //关闭按钮
@@ -33,11 +34,13 @@
         //var color_box = 'color-box';
         //hex-panel
         this.color_hex_panel = 'color-hex-panel';
+        this.color_hex_bg = 'color-hex-bg';
         //rgba
         this.rgba = 'RGBA';
         // rgba row
         this.color_hex_row = 'color-hex-row';
         //hex type  
+        this.color_hex_key = 'color-hex-key';
         this.color_hex_type = 'color-hex-type';
         //hex value
         this.color_hex_value = 'color-hex-value';
@@ -61,10 +64,21 @@
 
         this.COLOR_BOARD_SIZE = 256;
 
+        this.R = 255;
+        this.G = 0;
+        this.B = 0;
+
+        this.rgbMap = {
+            r: 255,
+            g: 255,
+            b: 255
+        }; //起点色值;
+
         var params = colorExtends(defaults, options);
 
-        this.left = parseInt(this.dom.offsetLeft); 
+        this.left = parseInt(this.dom.offsetLeft);
         this.top = parseInt(this.dom.offsetTop) + params.targetHeight;
+        //指针宽度的一半
         this.pointerSize = 8;
         //构建ui
         this.colorPickerUI = this.buildColorPickerHtml(params);
@@ -82,6 +96,8 @@
         // build color picker ui
         buildColorPickerHtml: function (params) {
 
+            var self = this;
+
             //外部容器
             var colorWrapper = buildDOM('div');
             colorWrapper.className = this.color_wrapper;
@@ -89,6 +105,7 @@
             //title
             var colorTitle = buildDOM('div');
             colorTitle.className = this.color_title;
+            colorTitle.textContent = 'colo picker';
 
             //关闭按钮
             var colorClosed = buildDOM('span');
@@ -96,12 +113,74 @@
 
             colorClosed.onclick = function () {
                 document.body.removeChild(colorWrapper);
+                colorWheel.style.left = 0;
+                colorWheel.style.top = 0;
+
+                self.left = parseInt(self.dom.offsetLeft);
+                self.top = parseInt(self.dom.offsetTop) + params.targetHeight;
+
+                colorWrapper.style.left = self.left + 'px';
+                colorWrapper.style.top = self.top + 'px';
+
             };
 
 
             colorTitle.appendChild(colorClosed);
-            this.dragMove(colorTitle, colorWrapper, 492, 0, 0);
 
+            if (params.dragMoveWrapper) {
+                wrapperDragMove();
+            }
+
+            function wrapperDragMove() {
+
+                var disX = 0;
+                var disY = 0;
+
+                colorTitle.onmousedown = function (e) {
+
+                    disX = e.pageX - self.left;
+                    disY = e.pageY - self.top;
+
+                    document.addEventListener('mousemove', onMouseMoveInWrapper, false);
+                    document.addEventListener('mouseup', onMouseUpInWrapper, false);
+
+
+                    function onMouseMoveInWrapper(e) {
+                        var left = e.pageX - disX;
+                        var top = e.pageY - disY;
+                        var maxL = document.documentElement.clientWidth - colorWrapper.clientWidth - 2;
+                        var maxT = document.documentElement.clientHeight - colorWrapper.clientHeight - 2;
+                        if (left < 0) {
+                            left = 0;
+                        } else if (left > maxL) {
+                            left = maxL
+                        }
+
+                        if (top < 0) {
+                            top = 0;
+                        } else
+                            if (top > maxT) {
+                                top = maxT;
+                            }
+
+
+                        self.left = left;
+                        self.top = top;
+
+                        colorWrapper.style.left = left + 'px';
+                        colorWrapper.style.top = top + 'px';
+
+                        return false;
+                    }
+
+                    function onMouseUpInWrapper() {
+                        document.removeEventListener('mousemove', onMouseMoveInWrapper);
+                        document.removeEventListener('mouseup', onMouseUpInWrapper);
+                    }
+
+                };
+
+            }
 
             //色板遮召层父级盒子
             var colorMaskParent = buildDOM('div');
@@ -115,17 +194,281 @@
             //色彩层
             var colorMask = buildDOM('div');
             colorMask.className = this.color_mask;
+            //default bgcolor #ff0000 rgba(255, 0, 0, 1);
+            colorMask.style.background = 'rgb(255, 0, 0)';
+
             //鼠标滚动层
             var colorWheel = buildDOM('div');
             colorWheel.className = this.color_wheel;
 
-            colorWheel.style.left = 0;
-            colorWheel.style.top = 0;
+            // colorWheel.style.left = 0;
+            // colorWheel.style.top = 0;
 
-            var w = parseInt(colorWheel.style.offsetWidth);
-            this.dragMove(colorWheel, colorWheel, this.COLOR_BOARD_SIZE, 5, 45);
+            // var w = parseInt(colorWheel.style.offsetWidth);
+
+            var colorbar = buildDOM('div');
+            colorbar.className = this.color_bar;
+
+            var colorSelectPointer = buildDOM('div');
+            colorSelectPointer.className = this.color_selected;
+
+            colorbar.appendChild(colorSelectPointer);
+
+            colorbar.onmousedown = function (e) {
+
+                var colorbarTop = self.top + colorbar.offsetTop;
+
+                // console.log(e.pageY);
+
+                var y = e.pageY - colorbarTop - 4;
+
+                //更新指针为止，更新色板颜色，更新rgba                
+
+                var rHex = document.getElementsByClassName(self.rgba[0].toLowerCase() + 'hex')[0];
+                var gHex = document.getElementsByClassName(self.rgba[1].toLowerCase() + 'hex')[0];
+                var bHex = document.getElementsByClassName(self.rgba[2].toLowerCase() + 'hex')[0];
+
+                colorSelectPointer.style.top = y + 'px';
+
+                self.B = y * 8;
+
+                if (self.B >= 255) {
+                    self.B = 255;
+
+                    if (self.R >= 255) {
+                        self.R = 255;
+
+                        self.G = y * 8;
+
+                        if (self.G >= 255) {
+                            self.G = 255;
+                        } else if (self.G <= 0) {
+                            self.G = 0;
+                        } else {
+                            self.G = y * 8;
+                        }
+
+                    } else if (self.R <= 0) {
+                        self.R = 0;
+                    } else {
+                        self.R = y * 8;
+                    }
+
+                } else if (self.B <= 0) {
+                    self.B = 0;
+                }
+                colorMask.style.background = 'rgb(' + self.R + ',' + self.B + ', 0)';
+
+                rHex.value = self.R;
+                gHex.value = self.G;
+                bHex.value = self.B;
 
 
+                document.addEventListener('mousemove', onMouseMoveSelect, false);
+                document.addEventListener('mouseup', onMouseUpSelect, false);
+
+                function onMouseMoveSelect(e) {
+
+                    y = e.pageY - self.top - colorbar.offsetTop - 4;
+
+                    if (y < -3) {
+                        y = -3;
+                    } else if (y > (self.COLOR_BOARD_SIZE - 3)) {
+                        y = self.COLOR_BOARD_SIZE - 3;
+                    }
+
+
+                    colorSelectPointer.style.top = y + 'px';
+
+                    colorMask.style.background = 'rgb(' + self.R + ',' + self.B + ', 0)';
+                    self.B = y * 8;
+
+                    // if (self.B >= 255) {
+                    //     self.B = 255;
+                    //     self.R = 255 - y;
+                    // } else if (self.B <= 0) {
+                    //     self.B = 0;
+                    // }
+
+                    if (self.B >= 255) {
+                        self.B = 255;
+
+                        if (self.R >= 255) {
+                            self.R = 255;
+
+                            self.G = y * 8;
+
+                            if (self.G >= 255) {
+                                self.G = 255;
+                            } else if (self.G <= 0) {
+                                self.G = 0;
+                            } else {
+                                self.G = y * 8;
+                            }
+
+                        } else if (self.R <= 0) {
+                            self.R = 0;
+                        } else {
+                            self.R = y * 8;
+                        }
+
+                    } else if (self.B <= 0) {
+                        self.B = 0;
+                    }
+
+                    rHex.value = self.R;
+                    gHex.value = self.G;
+                    bHex.value = self.B;
+
+                }
+
+                function onMouseUpSelect() {
+                    document.removeEventListener('mousemove', onMouseMoveSelect);
+                    document.removeEventListener('mouseup', onMouseUpSelect);
+                }
+
+            };
+
+
+
+            var colorPanel = buildDOM('div');
+            colorPanel.className = this.color_hex_panel;
+
+            var colorBg = buildDOM('div');
+            colorBg.className = this.color_hex_bg;
+            colorBg.style.background = 'rgb(255,255,255)';
+
+            colorPanel.appendChild(colorBg);
+
+            for (var i = 0; i < this.rgba.length; i++) {
+
+                var colorRgbRow = buildDOM('div');
+                colorRgbRow.className = this.color_hex_row;
+
+                var colorRgbKey = buildDOM('span');
+                colorRgbKey.className = this.color_hex_key;
+                colorRgbKey.textContent = this.rgba[i].toUpperCase() + ':';
+
+                var colorRgbValue = buildDOM('input');
+                colorRgbValue.className = this.color_hex_value + ' ' + this.rgba[i].toLowerCase() + 'hex';
+                colorRgbValue.type = 'text';
+                colorRgbValue.value = 0;
+
+                colorRgbRow.appendChild(colorRgbKey);
+                colorRgbRow.appendChild(colorRgbValue);
+
+                colorPanel.appendChild(colorRgbRow);
+
+            }
+
+
+            // var map = [-8, -8]; //起点
+            // var mapColor = [255, 0, 0];
+
+            colorBoard.onmousedown = wheelDragMove;
+            colorWheel.onmousedown = wheelDragMove;
+
+            function wheelDragMove(e) {
+
+                var disX = e.pageX - self.left - 6 - self.pointerSize; //滚轮至色板左边的距离
+                var disY = e.pageY - self.top - 47 - self.pointerSize;
+
+                colorWheel.style.left = disX + 'px';
+                colorWheel.style.top = disY + 'px';
+
+                // var map = self.rgbMap;
+
+                var r = self.rgbMap.r,
+                    g = self.rgbMap.g,
+                    b = self.rgbMap.b;
+
+                //如果disx <= -8;起点, r = 0;
+                //如果disx >= 256;终点
+
+                var hexL = parseInt(colorWheel.style.left) + self.pointerSize;
+                var hexT = parseInt(colorWheel.style.top) + self.pointerSize;
+
+                if (hexL === 256) hexL = self.R;
+                if (hexT === 256) hexT = self.R;
+
+                r = Math.abs(self.R - hexT);
+                if (hexL > hexT) {
+                    g = Math.abs(self.R - hexL);
+                    b = Math.abs(self.R - hexL);
+                } else {
+                    g = Math.abs(self.R - hexT);
+                    b = Math.abs(self.R - hexT);
+                }
+
+                var rgb = r + ',' + g + ',' + b;
+
+                colorBg.style.background = 'rgb(' + rgb + ')';
+
+                var rHex = document.getElementsByClassName(self.rgba[0].toLowerCase() + 'hex')[0];
+                var gHex = document.getElementsByClassName(self.rgba[1].toLowerCase() + 'hex')[0];
+                var bHex = document.getElementsByClassName(self.rgba[2].toLowerCase() + 'hex')[0];
+
+                rHex.value = r;
+                gHex.value = g;
+                bHex.value = b;
+
+
+                document.onmousemove = function (e) {
+
+                    disX = e.pageX - self.left - 6 - self.pointerSize;
+                    disY = e.pageY - self.top - 47 - self.pointerSize;
+
+                    console.log(disX, disY);
+
+                    if (disX < -self.pointerSize) {
+                        disX = -self.pointerSize;
+                    } else if (disX > self.COLOR_BOARD_SIZE - self.pointerSize) {
+                        disX = self.COLOR_BOARD_SIZE - self.pointerSize;
+                    }
+
+                    if (disY < -self.pointerSize) {
+                        disY = -self.pointerSize;
+                    } else if (disY > self.COLOR_BOARD_SIZE - self.pointerSize) {
+                        disY = self.COLOR_BOARD_SIZE - self.pointerSize;
+                    }
+
+                    colorWheel.style.left = disX + 'px';
+                    colorWheel.style.top = disY + 'px';
+
+                    hexL = parseInt(colorWheel.style.left) + self.pointerSize;
+                    hexT = parseInt(colorWheel.style.top) + self.pointerSize;
+
+                    if (hexL === 256) hexL = self.R;
+                    if (hexT === 256) hexT = self.R;
+
+                    r = Math.abs(self.R - hexT);
+
+                    if (hexL > hexT) {
+                        g = Math.abs(self.R - hexL);
+                        b = Math.abs(self.R - hexL);
+                    } else {
+                        g = Math.abs(self.R - hexT);
+                        b = Math.abs(self.R - hexT);
+                    }
+
+
+                    rgb = r + ',' + g + ',' + b;
+
+                    colorBg.style.background = 'rgb(' + rgb + ')';
+
+                    rHex.value = r;
+                    gHex.value = g;
+                    bHex.value = b;
+
+                    return false;
+
+                };
+
+                document.onmouseup = function () {
+                    document.onmousemove = null;
+                    document.onmouseup = null;
+                };
+            }
 
             colorMaskParent.appendChild(colorBoard);
             colorMaskParent.appendChild(colorMask);
@@ -133,14 +476,17 @@
 
             colorWrapper.appendChild(colorTitle);
             colorWrapper.appendChild(colorMaskParent);
+            colorWrapper.appendChild(colorbar);
+            colorWrapper.appendChild(colorPanel);
 
-            colorWrapper.style.width = params.wrapperWidth + 'px';
+            // colorWrapper.style.width = params.wrapperWidth + 'px';
             colorWrapper.style.left = this.left + 'px';
             colorWrapper.style.top = this.top + 'px';
 
             return colorWrapper;
 
         },
+
         initColorPicker: function (options) {
 
             //show color picker ui;
@@ -151,68 +497,19 @@
                 document.body.appendChild(self.colorPickerUI);
 
             };
-        },
-
-
-
-        dragMove: function (dragTarget, moveTarget, scope, x, y) {
-            // dragTarget: 触发事件目标
-            // moveTarget: 移动目标
-            // scope: 拖拽边界
-
-            var disx = 0,
-                disy = 0;
-                
-            var self = this;
-            
-            dragTarget.addEventListener('mousedown', onMouseDown, false);
-
-            function onMouseDown(e) {
-                disx = e.pageX - self.left + x;
-                disy = e.pageY - self.top + y;
-
-                document.addEventListener('mousemove', onMouseMove, false);
-                document.addEventListener('mouseup', onMouseUp, false);
-
-            }
-
-            function onMouseMove(e) {
-
-                var left = e.pageX - disx - self.left + x;
-                var top = e.pageY - disy - self.top + y;
-
-                var maxL = scope - disx + self.pointerSize/2 + x;
-                var maxT = scope - disy + self.pointerSize/2  + y;
-                console.log(disy);
-                if (left < - self.pointerSize) {
-                    left = - self.pointerSize;
-                } else if (left > maxL) {
-                   left = maxL; 
-                }
-                if (top < - self.pointerSize) {
-                    top = - self.pointerSize;
-                } else if (top > maxT) {
-                    top = maxT;
-                }
-
-                moveTarget.style.left = left + 'px';
-                moveTarget.style.top = top + 'px';
-
-                return false;
-
-            }
-
-            function onMouseUp() {
-
-                document.removeEventListener('mousemove', onMouseMove);
-                document.removeEventListener('mouseup', onMouseUp);
-
-            }
-
         }
 
     };
 
+
+    function toRgbHex(color) {
+
+        if (color.length < 2) {
+            return '0' + color.toString(16);
+        }
+
+        return color.toString(16);
+    }
 
 
     // 辅助函数
@@ -236,14 +533,7 @@
 
         return document.createElement(dom);
 
-        // this.dom = document.createElement(dom);
     }
-
-    // BuildDOM.prototype = {
-
-    //     setV  
-
-    // };
 
     function toDouble(num) {
 
