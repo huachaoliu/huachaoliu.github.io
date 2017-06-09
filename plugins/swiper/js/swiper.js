8b9d55f73f1aa99bff853d9e1759d2ecb07036c9
刘华超 <huachao.liu@daxiangyun.com>
2017-06-09 11:43:26 +0800

    add swiper.js

--- plugins/swiper/js/swiper.js
+++ plugins/swiper/js/swiper.js
@@ -1,268 +1,166 @@
-(function () {
-    'use strict';
-    var UI = {};
-
-    UI.Element = function (dom) {
-        this.dom = dom;
-    };
-
-    UI.prototype = {
-
-        add: function () {
-
-            for (var i = 0; i < arguments.length; i++) {
-
-                var arg = arguments[i];
-
-                if (arg instanceof UI.Element) {
-
-                    this.dom.appendChild(arg.dom);
-
-                } else {
-
-                    console.log( arg + 'is not instanceof of UI.Element')
-
-                }
-
-            }
-
-            return this;
-
-        },
-
-        remove: function () {
-
-            for (var i = 0; i < arguments.length; i++) {
-
-                var arg = arguments[i];
-
-                if (arg instanceof UI.Element) {
-
-                    this.dom.removeChild(arg.dom);
-
-                } else {
-
-                    console.log( arg + 'is not instanceof of UI.Element')
-
-                }
-
-            }
-
-            return this; 
-
-        },
-
-        getId: function () {
-
-            return this.dom.id;
-
-        },
-
-        getClass: function () {
-            
-            return this.dom.className;
-
-        },
-
-        setId: function (id) {
-
-            this.dom.id = id;
-
-            return this;
-
-        },
-
-        setClass: function (className) {
-
-            this.dom.className = className;
-
-            return this;
-
-        },
-
-        addClass: function (name) {
-
-            this.dom.className += this.dom.className.length > 0 ? ' ' + name : name;
-
-            return this;
-        },
-
-        removeClass: function (name) {
-
-            var classList = this.dom.className.split(" ");
-
-            for (var i = 0; i < classList.length; i++) {
-
-                if (classList[i] === name) {
-
-                    classList.splice(i, 1);
-                
-                }
-
-            }
-            this.dom.className = classList.join(" ");
-
-            return this;
-        },
-
-
-    };
-
-    UI.Div = function () {
-
-        UI.Element.call(this);
-
-        var dom = document.createElement('div');
-
-        this.dom = dom;
-
-        return this;
-
-    };
-
-    UI.Div.prototype = Object.create(UI.Element.prototype);
-    UI.Div.prototype.constructor = UI.Div;
-
-    UI.Text = function () {
-
-        UI.Element.call(this);
-
-        var dom = document.createElement('p');
-
-        this.dom = dom;
-
-        return this;
-
-    };
-
-    UI.Text.prototype = Object.create(UI.Element.prototype);
-    UI.Text.prototype.constructor = UI.Text;
-
-    UI.Text.prototype.setText = function (text) {
-
-        this.dom.textContent = text;
-
-        return this;
-
-    };
-
-    UI.Tag = function () {
-
-        UI.Element.call(this);
-
-        var dom = document.createElement('a');
-
-        this.dom = dom;
-
-        return this;
-
-    };
-
-    UI.Tag.prototype = Object.create(UI.Element.prototype);
-    UI.Tag.prototype.constructor = UI.Tag;
-
-    UI.Tag.prototype.constructor.setTarget = function (target) {
-
-        this.dom.href = target;
-
-        return this;
-
-    };
-
-
-    var Swiper = function (container, params) {
-        /**
-         * @param: {defaults} 配置参数
-         *  width: 可配置宽
-         *  height: 可配置高
-         *  loop: 动画循环
-         *  autoPlay: 自动播放
-         *  prevBtn: 左箭头
-         *  nextBtn: 右箭头
-         *  pagination: 底部圆点控制器
-        */
+function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(["jquery"], factory);
+    } else {
+        factory(jQuery)
+    }
+})(function ($) {
+    var Swiper = function (options) {
+        //plugins style
+        //TODO: 可配置的ui。
+        var stype = ['slider', 'gradient'];
+        var swiper_ui = {};
         var defaults = {
-            width: '100vw',
-            // height: '100vh',
-
-            //图片宽高比
-            SCALE: ((8 / 10 * 100) | 0) / 100,
-
+            n: 0,
             timer: null,
-            index: 0,
-
-            size: 3, //轮播图个数
-
-            loop: false,
-            autoPlay: false,
-            autoPlayDelay: '1000',
-
-            touchEventsTarget: 'container',
-            initialSlide: 0,
-            speed: 300,
-
-            src: [],
-
-            //文字覆盖
-            mask: false,
-
-            initMask: false,
-
-            //className
-            containerClass: 'swiper',
-            slideClass: 'swiper-slide',
-            slideItemClass: 'swiper-slide-item',
-            slideActiveClasS: 'swiper-slide-active',
-            slidePrevClass: 'swiper-slide-prev',
-            slideNextClass: 'swiper-slide-next'
-
+            speed: 3000,
+            style: stype,
+            swp: $('.swiper'),
+            swpW: $(window).width(),            
+            swpList: $('.swiper-list'),
+            swpNode: $('.swiper-list-node'),
+            arrow: $('.swiper-btn'),
+            prev: $('.swiper-btn-prev'),
+            next: $('.swiper-btn-next'),
+            btn: $('.swiper-index-child'),
+            nodeW: $(window).width(),
+            height: $(window).height(),
+            len: 3
         };
-
-        this.options = this.extend(defaults, params);
-
-        this.init(container);
-
-    };
-
+        var opt = $.extend(defaults, options);
+        this.init(opt);
+    }
     Swiper.prototype = {
-        
-        constructor: Swiper,
-
-        extend: function (target, options) {
-
-            for (var prop in target) {
-
-                if (options.hasOwnProperty(prop)) {
-
-                    target[prop] = options[prop];
-
-                }
-
+        init: function (opt) {
+            var ua = this.useAgent();
+            this.layout(opt);
+            switch(ua) {
+                case 'ios':
+                    this.mobileStart(opt);
+                    break;
+                case 'android':
+                     this.mobileStart(opt);
+                    break;
+                case 'pc':
+                     this.PcStart(opt);
+                    break;
+                default:                   
+                    break;
             }
-
-            return target;
-
         },
+        useAgent: function () {
+            var ua = window.navigator.userAgent;
+            var ipad = ua.match(/iPad/g);
+            var iphone = ua.match(/iPhone/g);
+            var ipod = ua.match(/iPod/g);            
+            var android = ua.match(/Android/g);            
+            if (ipad != null || iphone != null || ipod != null) {
+                return 'ios';
+            } else if (android != null) {
+                return 'android';
+            } else {
+                return 'pc';
+            }
+        },
+        layout: function (opt) {
+            opt.swp.width(opt.swpW);
+            opt.swp.height(opt.height);
+            opt.swpNode.width(opt.nodeW);
+            opt.arrow.css('lineHeight', opt.height+'px');
+            opt.swpList.width(opt.nodeW * opt.len);
+        },
+        PcStart: function (opt) {
+            var self = this;
+            opt.btn.on('click', function () {
+                var _this = $(this);
+                var index = _this.index();
+                if (_this.hasClass('swiper-active')) {
+                    return false;
+                } else {
+                    opt.btn.removeClass('swiper-active');
+                    _this.addClass('swiper-active');
+                    var _left = parseInt(opt.swpNode.width()) * index;
+                    opt.swpList.animate({
+                        left: -_left
+                    });
+                }
+            });
+            opt.prev.on('click', function () {
+                opt.n++;
+                if (opt.n > opt.len - 1) opt.n = 0;
+                self.move(opt);
+            });
 
-        init: function (container) {
+            opt.next.on('click', function () {
+                opt.n--;
+                if (opt.n < 0) opt.n = opt.len-1;
+                self.move(opt);
+            });
 
-
-            var opts = this.options;
-
-            var dom = document.getElementById(container);
-
-            var swiperSlide = new UI.Div();
-
-            swiperSlide.dom.className = opts.slideClass;
-
-            console.log(opts);
-
-            dom.appendChild(swiperSlide.dom);
-
+            function autoPlay () {
+                opt.n++;
+                if (opt.n > opt.len - 1) opt.n = 0;
+                opt.btn.eq(opt.n).trigger('click');
+            }
+            opt.timer = setInterval(autoPlay, opt.speed);
+            opt.swp.hover(function () {
+                clearInterval(opt.timer);
+            }, function () {
+                opt.timer = setInterval(autoPlay, opt.speed);
+            });
+        },
+        mobileStart: function (opt) {
+            var self = this;
+            opt.arrow.css('display', 'none');
+            var startX;
+            var touchX;
+            opt.swp.on('touchstart', function (e) {
+                clearInterval(opt.timer);
+                e.preventDefault();
+                startX = e.originalEvent.changedTouches[0].pageX;
+                return false;
+            });
+            opt.swp.on('touchmove', function (e) {
+                e.preventDefault();
+                touchX = e.originalEvent.changedTouches[0].pageX;
+            });
+            opt.swp.on('touchend', function (e) {
+                e.preventDefault();
+                if (touchX - startX > 0) {
+                     opt.n++;
+                    if (opt.n > opt.len - 1) {
+                        opt.n = 0;
+                        self.move(opt);
+                    } else {
+                        return false;
+                    }
+                } else {
+                    opt.n--;
+                    if (opt.n < 0) {
+                        opt.n = opt.len-1;
+                        self.move(opt);
+                    } else {
+                        return false;
+                    }
+                }
+                opt.timer = setInterval(autoPlay, opt.speed);
+            });
+            function autoPlay () {
+                opt.n++
+                if (opt.n > opt.len - 1) opt.n = 0;
+                self.move(opt);
+            }
+            opt.timer = setInterval(autoPlay, opt.speed);
+        },
+        move: function (opt) {
+            opt.btn.removeClass('swiper-active');
+            opt.btn.eq(opt.n).addClass('swiper-active');
+            opt.swpList.animate({
+                left: -parseInt(opt.swpNode.width()) * opt.n
+            });
         }
-
+    }
+    $.fn.swiper = function (options) {
+        return new Swiper(options);
     };
-
-    window.Swiper = Swiper;
-
-})();
+});
